ebe7b5016ddfe926dcd91673714999f96fdfd098
GANGA-N-L <nlganga2013@gmail.com>
2026-02-18 06:33:24 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="emergency_first_aid_chatbot",
+    version="0.1.0",
+    author="Ganga N L",
+    author_email="nlganga007@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
